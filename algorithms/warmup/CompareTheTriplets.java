8ddce0619a9139489290e9c430b214931ebc2f6e
Oliver Knowles <oliverknowles1@gmail.com>
2017-09-10 17:33:29 +0100

    Added some warmup solutions

---
+++ algorithms/warmup/CompareTheTriplets.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        int aliceScore = isLarger(a0, b0) + isLarger(a1, b1) + isLarger(a2, b2);
+        int bobScore = isLarger(b1, a1) + isLarger(b1, a1) + isLarger(b2, a2);
+        
+        return new int[] {aliceScore, bobScore};
+    }
+    
+    static int isLarger(int score1, int score2) {
+        return (score1 > score2) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+    }
+}
